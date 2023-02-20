adf328130586c920e4647f4b95a8c9d139e58f83
Jack Bauer <jackbauer.work@gmail.com>
2023-02-19 21:53:39 -0500

    Listening Lab

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
-function addingEventListener() {
-}
+function addingEventListener() {}
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Listening to Nodes</title>
+      <style>
+       div {
+         border: 1px solid red;
+         padding: 6px;
+         min-height: 12px;
+       }
+      </style>
+    </head>
+    <body>
+      <main id="main">My ID is "main"!</main>
+      <br />
+      <div>
+        <const> const input = document.getElementById('button');
+    
+          function clickAlert() {
+            alert('I was clicked!');
+          }
+          input.addEventListener('click', clickAlert);</const>
+      
+      </div>
+      <br />
+      <div>
+        1
+        <div>
+          2
+          <div>
+            3
+            <div>
+              4
+              <div>
+                5
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <script type="text/javascript" src="index.js"></script>
+    </body>
+    </html>
+    
